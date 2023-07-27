c8cbce69917f4af8aaa421ad61b02dadff1bdba1
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-07-26 21:43:14 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import { Mumbai } from "@thirdweb-dev/chains"
+import { Ethereum } from "@thirdweb-dev/chains"
 import { Navbar } from "../components/Navbar/Navbar";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={Mumbai}
+    <ThirdwebProvider activeChain={Ethereum}
       clientId={process.env.THIRDWEB_CLIENTID}
     >
       <Navbar />
